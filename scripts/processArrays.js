cc1239912d4aeb584fe8374447bd2ae9eb69e56b
tobytwigger <tt15951@my.bristol.ac.uk>
2018-02-18 20:44:00 +0000

    Added (and updated) feature to correctly capitalise names returned from unioncloud

--- scripts/processArrays.js
+++ scripts/processArrays.js
@@ -1,9 +1,9 @@
-var caps = require('string-capitalize-name');
+var caps = require('./string-capitalize-name');
 module.exports = function(response) {
 	if("forename" in response[0]){
-		response[0]["forename"] = caps(response[0]["forename"]);
+		response[0]["forename"] = caps(response[0]["forename"],{ignoreLessThanLength: 3});
 	} if("surname" in response[0]){
-		response[0]["surname"] = caps(response[0]["surname"]);
+		response[0]["surname"] = caps(response[0]["surname"],{ignoreLessThanLength: 3});
 	} 
 	return response;
 }
